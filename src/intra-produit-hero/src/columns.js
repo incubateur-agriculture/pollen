9a438af956daee547a0372d653b2e44e5c771f3a
Charline <charline.laporte@beta.gouv.fr>
2026-05-22 18:29:48 +0200

    feat(Hero): ajout du custom widget (#8)

---
+++ src/intra-produit-hero/src/columns.js
@@ -0,0 +1,28 @@
+const columns = [
+  {
+    name: 'nom',
+    description: 'Nom du produit',
+  },
+  {
+    name: 'dateCreation',
+    description: 'Date de création',
+  },
+  {
+    name: 'montantAE',
+    description: 'Montant AE',
+  },
+  {
+    name: 'montantCP',
+    description: 'Montant CP',
+  },
+  {
+    name: 'comiteMontantValide',
+    description: 'Montant validé par le comité',
+  },
+  {
+    name: 'comiteLien',
+    description: 'Lien des comités',
+  },
+]
+
+export default columns
