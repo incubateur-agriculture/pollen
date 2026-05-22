8cd279f5c150067e88876ff859d90ff66a0cffc9
Charline Laporte <charline.laporte@beta.gouv.fr>
2026-05-22 18:32:28 +0200

    Définition des colonnes utilisées pour le widgets

---
+++ src/intra-bdc-liste/src/columns.js
@@ -0,0 +1,32 @@
+const columns = [
+  {
+    name: 'nom',
+    description: 'Nom du bon de commande',
+  }, 
+  {
+    name: 'attributaire',
+    description: 'Attributaire',
+  },
+  {
+    name: 'marche',
+    description: 'Marche',
+  },
+  {
+    name: 'montantTotal',
+    description: 'Montant total',
+  },
+  {
+    name: 'montantConsomme',
+    description: 'Montant consommé',
+  }, 
+  {
+    name: 'montantRestant',
+    description: 'Montant restant à consommer',
+  },
+  {
+    name: 'dateBdc',
+    description: 'Date du bon de commande',
+  }
+]
+
+export default columns
