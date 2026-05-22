ae3a3648e5ad454e038394d3bd88fa561a4a5cbc
Charline Laporte <charline.laporte@beta.gouv.fr>
2026-05-22 18:32:08 +0200

    Ajout de la librairie charts

--- src/intra-bdc-liste/src/main.js
+++ src/intra-bdc-liste/src/main.js
@@ -5,6 +5,10 @@ import '@gouvfr/dsfr/dist/utility/colors/colors.min.css'
 import '@gouvminint/vue-dsfr/styles'
 import VueDsfr from '@gouvminint/vue-dsfr'
 
+import '@gouvfr/dsfr-chart'
+import '@gouvfr/dsfr-chart/css'
+
+import '@styles/app-style.css'
 import App from './App.vue'
 
 const app = createApp(App)
