90be5e29d53b40f4a97f3ba4c7fa10c796d46571
Charline <charline.laporte@beta.gouv.fr>
2026-05-22 18:54:14 +0200

    feat(Infos bon de commande): ajout du custom widget (#10)

--- src/services/formatter.js
+++ src/services/formatter.js
@@ -1,3 +1,6 @@
-const formatMontant = (value) => value > 0 ? new Intl.NumberFormat('fr-FR').format(value) : '-'
+const formatMontant = (value) => {
+  const isNull = value === null || value === undefined || value === false
+  return isNull ? '-' : new Intl.NumberFormat('fr-FR').format(value)
+}
 
 export { formatMontant }
