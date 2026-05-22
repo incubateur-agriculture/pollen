aa9b2bf8ee4607245f6370a663ffe7f2a5a37461
Charline <charline.laporte@beta.gouv.fr>
2026-05-22 17:18:11 +0200

    feat: création de la structure des widgets customs en vue.js (#1)

---
+++ src/intra-produit-hero/src/App.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>Vue produit hero</h1>
+</template>
---
+++ vite.config.js
@@ -0,0 +1,58 @@
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url))
+
+const portMap = {
+  'intra-produit-hero': 5174,
+  'intra-bdc-liste': 5175,
+  'intra-bdc-infos': 5176,
+}
+
+const getAppConfig = (mode) => {
+  const appName = mode || 'intra-produit-hero'
+  const port = portMap[appName] || 5173
+
+  return {
+    root: `src/${appName}`,
+    outDir: `dist/${appName}`,
+    port: port
+  }
+}
+
+export default defineConfig(({ mode }) => {
+  const appConfig = getAppConfig(mode)
+
+  return {
+    base: "./",
+    plugins: [
+      vue({
+        template: {
+          compilerOptions: {
+            isCustomElement: (tag) => tag.endsWith('-chart') || tag === 'data-box'
+          }
+        }
+      })
+    ],
+    root: appConfig.root,
+    build: {
+      outDir: `../../${appConfig.outDir}`,
+      emptyOutDir: true
+    },
+    css: {
+      lightningcss: {
+        errorRecovery: true
+      }
+    },
+    server: {
+      port: appConfig.port,
+      strictPort: false
+    },
+    resolve: {
+      alias: {
+        '@': `${__dirname}${appConfig.root}/src`,
+      }
+    }
+  }
+})
