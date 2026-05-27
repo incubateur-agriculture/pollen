a1378de0c7653a1f3a87391526874d5d67d8212d
Charline Laporte <charline.laporte@beta.gouv.fr>
2026-05-27 15:56:18 +0200

    Ajoute une nouvelle colonne pour grouper les produits par statut

---
+++ src/intra-produit-selecteur/src/columns.js
@@ -0,0 +1,12 @@
+const columns = [
+  {
+    name: 'name',
+    description: 'Champ de la liste déroulante',
+  },
+  {
+    group: 'groupBy',
+    description: 'Grouper par',
+  }
+]
+
+export default columns
