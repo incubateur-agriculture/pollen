d307ce00ae55c3165865c5d3d70386055b2003a0
Charline Laporte <charline.laporte@beta.gouv.fr>
2026-05-27 16:17:16 +0200

    Supprime le groupBy pour la V1

--- src/intra-produit-selecteur/src/columns.js
+++ src/intra-produit-selecteur/src/columns.js
@@ -3,10 +3,6 @@ const columns = [
     name: 'name',
     description: 'Champ de la liste déroulante',
   },
-  {
-    group: 'groupBy',
-    description: 'Grouper par',
-  }
 ]
 
 export default columns
