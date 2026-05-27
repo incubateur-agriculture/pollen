0e09c2c11e7b3c48a603f0b0681c3ea2b7d51027
Charline Laporte <charline.laporte@beta.gouv.fr>
2026-05-27 15:56:41 +0200

    Initialisation de la nouvelle vue

---
+++ src/intra-produit-selecteur/src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import columns from './columns'
+import GristContainer from '@components/GristContainer.vue'
+</script>
+
+<template>
+  <GristContainer :columns="columns">
+    <h1 class="fr-h4">Sélecteur de produit</h1>
+  </GristContainer>
+</template>
